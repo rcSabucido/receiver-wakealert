5d991a147a7efbce53879122532b0a56711a0f27
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2026-04-22 21:23:42 +0800

    Initialize boilerplate files for wakealert receiver

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <h1 className="text-9xl font-bold text-red-900">HELLO WORLD</h1>
+    </div>
+  )
+}
+
+export default App
