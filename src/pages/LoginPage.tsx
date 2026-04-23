0b5bf3c04b8167b6fdbfb21bf4b38551ec55806b
Dan <danbap300@gmail.com>
2026-04-23 14:13:40 +0800

    #34 Revamp LoginPage layout and styling with improved form structure and labels

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -8,16 +8,60 @@ export function LoginPage() {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen w-screen bg-linear-to-br from-gray-900 to-gray-800">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold mb-6 text-gray-800">WakeAlert</h1>
-        <button
-          onClick={handleLogin}
-          className="w-full py-3 px-6 bg-blue-600 text-white rounded hover:bg-blue-700 transition font-semibold"
-        >
-          Login
-        </button>
+    <div className="flex h-screen w-full font-sans overflow-hidden">
+
+      <div className="hidden md:flex w-1/2 items-center justify-center bg-white p-10">
+        <div className="flex flex-col items-center">
+          <div className="h-[30vw] w-[30vw] max-w-[500px] max-h-[500px] rounded-full 
+                        bg-blue-500 shadow-2xl flex items-center justify-center transition-all duration-500">
+          </div>
+        </div>
+      </div>
+
+      <div className="flex w-full md:w-1/2 items-start justify-start bg-blue-500 pt-[15vh] px-6 md:pl-19">
+        
+        <div className="w-full max-w-[738px]">
+          <h1 className="mb-7 text-4xl md:text-5xl lg:text-5xl font-semibold text-white tracking-tight">
+            Login
+          </h1>
+          
+          <form onSubmit={handleLogin} className="space-y-9">
+
+            <div className="flex flex-col">
+              <label className="block text-xl md:text-xl font-semibold text-white mb-3">
+                Username:
+              </label>
+              <input
+                required
+                type="text"
+                className="w-full md:h-12 rounded-xl p-4 shadow-xl focus:ring-2 
+                focus:ring-blue-300 outline-none text-gray-800 bg-white transition-all"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label className="block text-xl md:text-xl font-semibold text-white mb-3">
+                Password:
+              </label>
+              <input
+                required
+                type="password"
+                className="w-full md:h-12 rounded-xl p-4 shadow-xl focus:ring-2 
+                focus:ring-blue-300 outline-none text-gray-800 bg-white transition-all text-lg"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full md:h-12 rounded-xl bg-lime-200 font-bold
+            text-gray-800 shadow-lg text-xl mt-5 hover:bg-lime-300 transition-colors"
+            >
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
 }
+
+export default LoginPage;
